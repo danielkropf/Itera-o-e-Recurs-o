954c7651daa36a697b0816c2bd0e15cfc96a5aee
danielkropf <danielkropf@uol.com.br>
2015-03-06 09:47:58 -0300

    Trabalho de sala de aula Daniel Kropf e Luiz Roberto
    Versão final do trabalho

---
+++ Sisteminha/Sisteminha/Form1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Sisteminha
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        double num;
+        double result;
+
+        public double rec(double var)
+        {
+            if (var == 0)
+                return 1;
+
+            return var*rec(var - 1);
+        }
+
+        private void iteracao(object sender, EventArgs e)
+        {
+            num = Convert.ToDouble(texto.Text);
+            result = num;
+            for (int i = 0; i < (num - 1); i++) {
+                result = result * (num - (i + 1));
+            }
+            lbl.Text = Convert.ToString(result);
+        }
+
+        private void recursao(object sender, EventArgs e)
+        {
+            num = Convert.ToDouble(texto.Text);
+            lbl.Text = Convert.ToString(rec(num));
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
